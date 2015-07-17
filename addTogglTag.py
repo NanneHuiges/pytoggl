85335f6c570e623823955aef9ff47c1b3cdb4af2
Nanne <nanne.huiges@belsimpel.nl>
2015-07-17 15:01:30 +0200

    discovered "pep8": mostly whitespaceing, style

--- addTogglTag.py
+++ addTogglTag.py
@@ -15,84 +15,92 @@ from pytoggl import PyToggl
 # https://www.toggl.com/app/profile  #
 ######################################
 
-token = ""         
-entryName = "Entry started by addTogglTag script"  # the default description for a new entry.
+token = ""
+entryName = "Entry started by addTogglTag script"
 
 ###################################################
 #        No changes needed below this line        #
 ###################################################
 
 # internal vars reflecting commandline args
-verbose = False   
-tagname = None        
-workspace = None  
-create = True     
+verbose = False
+tagname = None
+workspace = None
+create = True
+
 
 def parseArguments():
-  """Parse the commandline arguments, and set the globals to reflect them """
-  global verbose, tagname, workspace, create
-  parser = argparse.ArgumentParser(description='Add tags to running Toggl '
-    'entries: will create a new one if none is running')
-  parser.add_argument("-v", "--verbose", help="Increase output verbosity",
-    action="store_true")
-  parser.add_argument("-t", "--tag", help="The name of the tag to add", 
-    required=True)
-  parser.add_argument("-w", "--workspace", 
-    help="The name of the workspace a new entry is started in; "
-    "default is the first encountered")
-  parser.add_argument("-n", "--entrydname", 
-    help="The name used for an entry, if a new one is created; "
-    "has no meaning when supplied with --dontcreate "
-    "default is '"+entryName+"'")
-  parser.add_argument("--dontcreate", 
-    help="Don't create a new entry if none is running", action="store_true")
-  args = parser.parse_args()
-  verbose = args.verbose
-  tagname = args.tag
-  workspace = args.workspace
-  if args.dontcreate:
-      create = False
+    """Parse the commandline arguments, and set the globals to reflect them """
+    global verbose, tagname, workspace, create
+    parser = argparse.ArgumentParser(
+        description='Add tags to running Toggl '
+                    'entries: will create a new one if none is running')
+    parser.add_argument("-v", "--verbose",
+                        help="Increase output verbosity",
+                        action="store_true")
+    parser.add_argument("-t", "--tag",
+                        help="The name of the tag to add",
+                        required=True)
+    parser.add_argument("-w", "--workspace",
+                        help="The name of the workspace a new entry is "
+                             "started in; default is the first encountered")
+    parser.add_argument("-n", "--entrydname",
+                        help="The name used for an entry, "
+                             "if a new one is created; "
+                             "has no meaning when supplied with --dontcreate "
+                             "default is '"+entryName+"'")
+    parser.add_argument("--dontcreate",
+                        help="Don't create a new entry if none is running",
+                        action="store_true")
+    args = parser.parse_args()
+    verbose = args.verbose
+    tagname = args.tag
+    workspace = args.workspace
+    if args.dontcreate:
+        create = False
+
 
 def log(msg):
-  if verbose:
-    print msg      
+    if verbose:
+        print msg
+
 
 #################################################
 def main():
-  parseArguments()
-  pt = PyToggl(token)
-  pt.setVerbose(verbose)
+    parseArguments()
+    pt = PyToggl(token)
+    pt.setVerbose(verbose)
 
-  current = pt.getCurrent()
-  if current is False:
-    log('Entry-retrieval went wrong. Exit!')
-    return 1
+    current = pt.getCurrent()
+    if current is False:
+        log('Entry-retrieval went wrong. Exit!')
+        return 1
 
-  if current is None:
-    if not create:
-        # no running entry & specified not to create one. We're done
-        log('No running entry and not creating one per request. Finished!')
-        return 0
+    if current is None:
+        if not create:
+            # no running entry & specified not to create one. We're done
+            log('No running entry and not creating one per request. Finished!')
+            return 0
 
-    #start new entry
+      #start new entry
     workspace = pt.getWorkspace()
     if not workspace:
-      log('Cannot find a workspace to create entry in. Exit!')
-      return 1
+        log('Cannot find a workspace to create entry in. Exit!')
+        return 1
 
-    #create entry 
-    current = pt.createEntry(workspace['id'], entryName);
+    #create entry
+    current = pt.createEntry(workspace['id'], entryName)
     if not current:
-      log('Failed creating a workspace. Exit!')
-      return 1
+        log('Failed creating a workspace. Exit!')
+        return 1
 
-  # Entry is running
-  tag = pt.createTag(current['wid'], tagname)
-  if not tag:
-    log('Failed finding or creating the tag. Exit!')
-    return 1
+    # Entry is running
+    tag = pt.createTag(current['wid'], tagname)
+    if not tag:
+        log('Failed finding or creating the tag. Exit!')
+        return 1
 
-  return pt.addTagToEntry(current, tag['name'])
+    return pt.addTagToEntry(current, tag['name'])
 
 if __name__ == "__main__":
     exit(main())
--- pytoggl.py
+++ pytoggl.py
@@ -15,149 +15,151 @@ author: Nanne Huiges
 email: nanne@huiges.nl
 """
 
-import requests, json
+import requests
+import json
 
 
 class PyToggl:
-  """A simple wrapper for the Toggl REST API"""
+    """A simple wrapper for the Toggl REST API"""
 
-  _log = []
-  _token = None
-  _baseurl = "https://www.toggl.com/api/v8/"
-  _verbose = False
+    _log = []
+    _token = None
+    _baseurl = "https://www.toggl.com/api/v8/"
+    _verbose = False
 
-  def __init__(self, token):
-    self._token = token
+    def __init__(self, token):
+        self._token = token
 
-  def setVerbose(self, verbose):
-    """Prints the log messages during execution"""
-    self._verbose = verbose
+    def setVerbose(self, verbose):
+        """Prints the log messages during execution"""
+        self._verbose = verbose
 
-  def getLog(self):
-    """Returns the log. The log gets set even if verbose is off"""
-    return self._log
-    
-  def _logMsg(self, msg):
-    if self._verbose:
-      print msg
-    self._log.append(msg)
+    def getLog(self):
+        """Returns the log. The log gets set even if verbose is off"""
+        return self._log
 
-  def _get(self, url):
-    r = requests.get(self._baseurl+url, auth=(self._token, 'api_token')) 
-    if not r.status_code == 200:
-      return False
-    return json.loads(r.text)  
+    def _logMsg(self, msg):
+        if self._verbose:
+            print msg
+        self._log.append(msg)
 
-  def _post(self, url, data):
-    r =  requests.post(self._baseurl+url, data, auth=(self._token, 'api_token'))
-    if not r.status_code == 200:
-      return False
-    return json.loads(r.text)  
+    def _get(self, url):
+        r = requests.get(self._baseurl+url,
+                         auth=(self._token, 'api_token'))
+        if not r.status_code == 200:
+            return False
+        return json.loads(r.text)
 
-  def _put(self, url, data):
-    r =  requests.put(self._baseurl+url, data, auth=(self._token, 'api_token'))     
-    if not r.status_code == 200:
-      return False
-    return json.loads(r.text)  
+    def _post(self, url, data):
+        r = requests.post(self._baseurl+url, data,
+                          auth=(self._token, 'api_token'))
+        if not r.status_code == 200:
+            return False
+        return json.loads(r.text)
 
-  def getCurrent(self):
-    """Get the currently running entry, if any
-    Returns false on error, None if nothing is running
-    and the entry if there is one.
-    """
-    self._logMsg('Getting current entry')
-    current = self._get("time_entries/current")
-    if not current:
-      self._logMsg("Could not retrieve running entry.")
-      return False
+    def _put(self, url, data):
+        r = requests.put(self._baseurl+url, data,
+                         auth=(self._token, 'api_token'))
+        if not r.status_code == 200:
+            return False
+        return json.loads(r.text)
 
-    if current['data'] is None:
-      self._logMsg("No running entry.")
-      return None
+    def getCurrent(self):
+        """Get the currently running entry, if any
+        Returns false on error, None if nothing is running
+        and the entry if there is one.
+        """
+        self._logMsg('Getting current entry')
+        current = self._get("time_entries/current")
+        if not current:
+            self._logMsg("Could not retrieve running entry.")
+            return False
 
-    self._logMsg('Entry found')
-    return current['data']
+        if current['data'] is None:
+            self._logMsg("No running entry.")
+            return None
 
+        self._logMsg('Entry found')
+        return current['data']
 
-  def getWorkspace(self, name=None):
-    """Get the workspace with specified name, or get the first one
-    Returns false on error, or returns a workspace
-    """
-    self._logMsg('Retrieving workspaces')
-    spaces = self._get("workspaces")
-    if not spaces:
-        self._logMsg("Could not find workspaces. Exiting")
+    def getWorkspace(self, name=None):
+        """Get the workspace with specified name, or get the first one
+        Returns false on error, or returns a workspace
+        """
+        self._logMsg('Retrieving workspaces')
+        spaces = self._get("workspaces")
+        if not spaces:
+            self._logMsg("Could not find workspaces. Exiting")
+            return False
+
+        if name is None:
+            self._logMsg('Using first workspace: '+spaces[0]['name'])
+            return spaces[0]
+
+        #specified a workspace
+        for space in spaces:
+            if space['name'] == name:
+                self._logMsg('Found specified workspace ' + name)
+                return space
+
+        self._logMsg("specified workspace "+name+" not found.")
         return False
 
-    if name is None:
-        self._logMsg('Using first workspace: '+spaces[0]['name'])
-        return spaces[0]
-    
-    #specified a workspace  
-    for space in spaces:
-        if space['name'] == name:
-            self._logMsg('Found specified workspace '+ name)
-            return space
+    def createTag(self, wid, name):
+        """Create a tag (or ignore if already created)
+        Returns false if tags cannot be retrieved, or tag cannot be made.
+        otherwise returns (new/found) the tag
+        """
+        self._logMsg('Creating tag '+name+', if needed')
+        tags = self._get("workspaces/"+str(wid)+"/tags")
+        if not tags:
+            self._logMsg('Cannot retreive workspace tags')
+            return False
 
-    self._logMsg("specified workspace "+name+" not found.")
-    return False
+        for tag in tags:
+            if tag['name'] == name:
+                self._logMsg('Tag already available')
+                return tag
 
-  def createTag(self, wid, name):
-    """Create a tag (or ignore if already created)
-    Returns false if tags cannot be retrieved, or tag cannot be made.
-    otherwise returns (new/found) the tag
-    """
-    self._logMsg('Creating tag '+name+', if needed')
-    tags = self._get("workspaces/"+str(wid)+"/tags")
-    if not tags:
-      self._logMsg('Cannot retreive workspace tags')
-      return False
+        self._logMsg('New tag. Creating')
+        data = json.dumps({"tag": {"name": name, "wid": wid}})
+        tag = self._post("tags", data)
+        if not tag:
+            self._logMsg('Creating tag failed')
+            return False
 
-    for tag in tags:
-      if tag['name'] == name:
-        self._logMsg('Tag already available')
-        return tag  
-    
-    self._logMsg('New tag. Creating') 
-    data = json.dumps({"tag":{"name":name,"wid":wid}})
-    tag = self._post("tags", data)
-    if not tag:
-        self._logMsg('Creating tag failed')
-        return False
+        self._logMsg('Tag created')
+        return tag['data']
 
-    self._logMsg('Tag created')
-    return tag['data']  
+    def createEntry(self, wid, description):
+        """Create a new entry
+        Starts a new entry with provided description
+        in provided workspace.
+        """
+        self._logMsg('Creating entry')
+        data = json.dumps({"time_entry":
+                          {"description": description,
+                           "wid": wid,
+                           "created_with": "PyToggl"
+                           }})
+        entry = self._post('time_entries/start', data)
+        if not entry:
+            self._logMsg('Could not start entry')
+            return False
 
-  def createEntry(self, wid, description):
-    """Create a new entry
-    Starts a new entry with provided description
-    in provided workspace.
-    """
-    self._logMsg('Creating entry');
-    data = json.dumps({"time_entry":
-      {"description":description,
-      "wid":wid, 
-      "created_with":"PyToggl"}
-      })      
-    entry = self._post('time_entries/start', data)
-    if not entry:
-      self._logMsg('Could not start entry')
-      return False
+        self._logMsg('Entry started')
+        return entry['data']
 
-    self._logMsg('Entry started')
-    return entry['data']
-  
+    def addTagToEntry(self, entry, name):
+        """Adds a tag to an entry"""
+        self._logMsg('Adding tag to entry')
+        tags = entry['tags']
+        tags.append(name)
+        data = json.dumps({"time_entry": {"tags": tags}})
+        res = self._put("time_entries/"+str(entry['id']), data)
+        if not res:
+            self._logMsg('Adding tag failed')
+            return False
 
-  def addTagToEntry(self, entry, name):
-    """Adds a tag to an entry"""
-    self._logMsg('Adding tag to entry');
-    tags = entry['tags']
-    tags.append(name) 
-    data = json.dumps({"time_entry":{"tags":tags}})    
-    res = self._put("time_entries/"+str(entry['id']),data)
-    if not res:
-      self._logMsg('Adding tag failed')
-      return False
-
-    self._logMsg('Tag added')
-    return True
+        self._logMsg('Tag added')
+        return True
